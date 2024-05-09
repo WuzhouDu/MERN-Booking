234ec0354973bf9c9e9bac3e5543475b9eabfe8d
WuzhouDu <duwuz@outlook.com>
2024-05-09 18:32:17 +0800

    backend: use express-validator to validate the register action

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,23 +1,34 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import User from '../models/user';
 import jwt from 'jsonwebtoken';
+import {check, validationResult} from 'express-validator';
 
 const userRouter = express.Router();
 
-userRouter.post("/register", async (req: Request, res: Response) => {
+userRouter.post("/register", [
+    check("firstName", "First name is required!").isString(),
+    check("lastName", "Last name is required!").isString(),
+    check("email", "email is required!").isEmail(),
+    check("password", "password with 6 or more chars is required!").isLength({ min: 6 }),
+
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({message: errors.array()})
+    }
     try {
         let user = await User.findOne({
             email: req.body.email,
         });
 
         if (user) {
-            return res.status(400).json({message: "User already exist"});
+            return res.status(400).json({ message: "User already exist" });
         }
 
         user = new User(req.body);
         await user.save();
 
-        const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET_KEY as string, {
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, {
             expiresIn: "1d",
         });
 
@@ -26,9 +37,9 @@ userRouter.post("/register", async (req: Request, res: Response) => {
             secure: process.env.NODE_ENV === "production",
             maxAge: 24 * 60 * 60 * 1000,
         });
-        return res.status(200);
+        return res.status(200).send();
     } catch (err) {
-        res.status(500).send({message: "Something Wrong"});
+        res.status(500).send({ message: "Something Wrong" });
     }
 })
 
