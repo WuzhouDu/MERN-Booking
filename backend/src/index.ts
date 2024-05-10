5fb1fbac6eef6befead38b437b0afd004040904f
WuzhouDu <duwuz@outlook.com>
2024-05-10 14:59:19 +0800

    fron-back: store the cookie in the browser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
