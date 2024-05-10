7fd77e09b0f9b389a7f52c33f023f9be93a65a97
WuzhouDu <duwuz@outlook.com>
2024-05-10 22:08:44 +0800

    test: add login and register test; fix: frontend register should invalidate token

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from '../api-client';
 import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
@@ -14,10 +14,12 @@ export type RegisterFormData = {
 const Register = () => {
     const navigate = useNavigate();
     const { showToast } = useAppContext();
+    const queryClient = useQueryClient();
     const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
     const mutation = useMutation(apiClient.register, {
         onSuccess: () => {
             showToast({ message: "Registration Success!", type: "SUCCESS" });
+            queryClient.invalidateQueries("validateToken");
             navigate('/');
         },
         onError: (error: Error) => {
