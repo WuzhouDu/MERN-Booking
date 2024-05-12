e968d7aa4d74bac4dd5ca9ffd80583b64ad76258
WuzhouDu <duwuz@outlook.com>
2024-05-12 21:08:30 +0800

    backend: add validators of hotel form

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import cloudinary from 'cloudinary';
 import Hotel, { HotelType } from '../models/hotel';
 import verifyToken from '../middleware/auth';
+import { body } from 'express-validator';
 
 const hotelRouter = express.Router();
 const storage = multer.memoryStorage();
@@ -16,7 +17,15 @@ const upload = multer({
 
 // api/my-hotels
 hotelRouter.post('/',
-    verifyToken,
+    verifyToken,[
+        body("name").notEmpty().withMessage("Name is required"),
+        body("city").notEmpty().withMessage("City is required"),
+        body("country").notEmpty().withMessage("Country is required"),
+        body("description").notEmpty().withMessage("Description is required"),
+        body("type").notEmpty().withMessage("Hotel type is required"),
+        body("pricePerNight").notEmpty().isNumeric().withMessage("PricePerNight is required and number"),
+        body("facilities").notEmpty().isArray().withMessage("Facilities is required and arry"),
+    ],
     upload.array("imageFiles", 6),
     async (req: Request, res: Response) => {
         try {
