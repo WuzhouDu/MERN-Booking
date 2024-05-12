0bf295820afe59d8176c01ed5c9643a9eb3abb56
WuzhouDu <duwuz@outlook.com>
2024-05-12 21:03:49 +0800

    backend: create hotel model; hotel router; use cloudinary to store images.

---
+++ backend/src/routes/my-hotels.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from 'express';
+import multer from 'multer';
+import cloudinary from 'cloudinary';
+import Hotel, { HotelType } from '../models/hotel';
+import verifyToken from '../middleware/auth';
+
+const hotelRouter = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB
+    }
+})
+
+
+// api/my-hotels
+hotelRouter.post('/',
+    verifyToken,
+    upload.array("imageFiles", 6),
+    async (req: Request, res: Response) => {
+        try {
+            const imageFiles = req.files as Express.Multer.File[];
+            const newHotel: HotelType = req.body;
+            // 1. upload images to cloudinary
+            const uploadPromise = imageFiles.map(async (image) => {
+                const b64 = Buffer.from(image.buffer).toString("base64");
+                let dataURI = "data:" + image.mimetype + ";base64," + b64;
+                const res = await cloudinary.v2.uploader.upload(dataURI);
+                return res.url;
+            });
+
+
+            // 2. if upload successful, create new hotel
+            const imageURLs = await Promise.all(uploadPromise);
+            newHotel.imageUrls = imageURLs;
+            newHotel.lastUpdated = new Date();
+            newHotel.usrId = req.userId;
+
+            // 3. save new hotel to db
+            const hotel = new Hotel(newHotel);
+            await hotel.save();
+
+            res.status(201).send(hotel);
+        } catch (e) {
+            console.log("Error creating hotel:", e);
+            res.status(500).json({ message: "Something went wrong" });
+        }
+    })
