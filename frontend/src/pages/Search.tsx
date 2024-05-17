69f871a4b60caf63a1e9ecf9111e7a2daccfbb6d
WuzhouDu <duwuz@outlook.com>
2024-05-17 20:59:25 +0800

    front: finish the search card

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { useSearchContext } from "../contexts/SearchContext"
 import * as apiClient from '../api-client';
 import { useState } from "react";
+import SearchResultsCard from "../components/SearchResultCard";
 
 const Search = () => {
     const search = useSearchContext();
@@ -18,7 +19,23 @@ const Search = () => {
     const { data: hotelData } = useQuery(["searchHotels", searchParams], async () => await apiClient.searchHotels(searchParams));
 
     return (
-        <>Search page</>
+        <div className="grid grid-cols-1 lg:grid-cols-[200px_1fr] gap-5">
+            <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+                <div className="space-y-5">
+                    <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">Filter by: todo</h3>
+                </div>
+            </div>
+            <div className="flex flex-col gap-5">
+                <div className="flex justify-between items-center">
+                    <span className="text-xl font-bold">
+                        {hotelData?.pagination.total} Hotels found {search.destination ? `in ${search.destination}` : ""}
+                    </span>
+                </div>
+                {hotelData?.data.map((hotel)=> (
+                    <SearchResultsCard key={hotel._id} hotel={hotel} />
+                ))}
+            </div>
+        </div>
     )
 }
 
