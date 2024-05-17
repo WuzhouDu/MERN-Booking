7523bf0b7764ab511752d7d3ad2ad8dbeb488525
WuzhouDu <duwuz@outlook.com>
2024-05-17 15:55:18 +0800

    add the toast.

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,11 +2,12 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useParams } from "react-router-dom"
 import * as apiClient from '../api-client';
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
 
 const EditHotel = () => {
     const { hotelId } = useParams();
     const queryClient = useQueryClient();
-
+    const {showToast} = useAppContext();
     const { data: hotel } = useQuery("fetchMyHotelById", () => apiClient.fetchMyHotelById(hotelId || ''), {
         enabled: !!hotelId // verify hotelId has a truth value
     });
@@ -14,8 +15,11 @@ const EditHotel = () => {
     const { mutate, isLoading } = useMutation(apiClient.updateHotelById, {
         onSuccess: () => {
             queryClient.invalidateQueries("fetchMyHotelById");
+            showToast({message: "Hotel saved!", type: "SUCCESS"});
         },
-        onError: () => { },
+        onError: () => { 
+            showToast({message: "Error saving hotel", type: "ERROR"});
+        },
     })
 
     const handleSave = (hotelFormData: FormData) => {
