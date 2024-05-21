74ee2b6b9755acf87d5ae58cbc7b90043a48ab00
WuzhouDu <duwuz@outlook.com>
2024-05-21 20:06:21 +0800

    front: initialize the booking form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import MyHotels from './pages/MyHotels'
 import EditHotel from './pages/EditHotel'
 import Search from './pages/Search'
 import Detail from './pages/Detail'
+import Booking from './pages/Booking'
 
 
 function App() {
@@ -35,6 +36,11 @@ function App() {
         <Route path='/sign-in' element={<Layout><SignIn /></Layout>} />
         {isLoggedIn && (
           <>
+            <Route path='/hotel/:hotelId/booking' element={
+              <Layout>
+                <Booking />
+              </Layout>}>
+            </Route>
             <Route path='/add-hotel' element={
               <Layout>
                 <AddHotel />
